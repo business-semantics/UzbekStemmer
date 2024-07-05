dec2391e3702df1a8c0584f0a731a9ce4665705a
Giuseppe Miscione <giuseppe.miscione@smartkyc.com>
2024-07-05 10:22:24 +0200

    Added plugin to generate source package. Simplified dependencies

--- src/main/java/com/smartkyc/stemmers/uzbek/Suffixes.java
+++ src/main/java/com/smartkyc/stemmers/uzbek/Suffixes.java
@@ -4,22 +4,28 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlText;
-import lombok.Getter;
-import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@Getter
-@Setter
 public class Suffixes
 {
 	@JacksonXmlProperty(localName = "suffixes")
 	@JacksonXmlElementWrapper(useWrapping = false)
 	private List<Suffix> suffixes = new ArrayList<>();
 
-	@Getter
-	@Setter
+	public List<Suffix> getSuffixes()
+	{
+		return this.suffixes;
+	}
+
+	@JacksonXmlElementWrapper(useWrapping = false)
+	@JacksonXmlProperty(localName = "suffixes")
+	public void setSuffixes(final List<Suffix> suffixes)
+	{
+		this.suffixes = suffixes;
+	}
+
 	public static class Suffix
 	{
 		@JacksonXmlProperty(isAttribute = true, localName = "class")
@@ -32,8 +38,40 @@ public class Suffixes
 		@JacksonXmlElementWrapper(useWrapping = false)
 		private List<Element> elements = new ArrayList<>();
 
-		@Getter
-		@Setter
+		public String getSuffixClass()
+		{
+			return this.suffixClass;
+		}
+
+		public String getClassId()
+		{
+			return this.classId;
+		}
+
+		public List<Element> getElements()
+		{
+			return this.elements;
+		}
+
+		@JacksonXmlProperty(isAttribute = true, localName = "class")
+		public void setSuffixClass(final String suffixClass)
+		{
+			this.suffixClass = suffixClass;
+		}
+
+		@JacksonXmlProperty(isAttribute = true, localName = "classId")
+		public void setClassId(final String classId)
+		{
+			this.classId = classId;
+		}
+
+		@JacksonXmlElementWrapper(useWrapping = false)
+		@JacksonXmlProperty(localName = "element")
+		public void setElements(final List<Element> elements)
+		{
+			this.elements = elements;
+		}
+
 		@JsonIgnoreProperties(ignoreUnknown = true)
 		public static class Element
 		{
@@ -49,8 +87,50 @@ public class Suffixes
 			@JacksonXmlProperty(localName = "pass")
 			private Pass pass;
 
-			@Getter
-			@Setter
+			public String getId()
+			{
+				return this.id;
+			}
+
+			public ElementSuffix getElementSuffix()
+			{
+				return this.elementSuffix;
+			}
+
+			public Join getJoin()
+			{
+				return this.join;
+			}
+
+			public Pass getPass()
+			{
+				return this.pass;
+			}
+
+			@JacksonXmlProperty(isAttribute = true, localName = "id")
+			public void setId(final String id)
+			{
+				this.id = id;
+			}
+
+			@JacksonXmlProperty(localName = "suffix")
+			public void setElementSuffix(final ElementSuffix elementSuffix)
+			{
+				this.elementSuffix = elementSuffix;
+			}
+
+			@JacksonXmlProperty(localName = "join")
+			public void setJoin(final Join join)
+			{
+				this.join = join;
+			}
+
+			@JacksonXmlProperty(localName = "pass")
+			public void setPass(final Pass pass)
+			{
+				this.pass = pass;
+			}
+
 			public static class ElementSuffix
 			{
 				@JacksonXmlProperty(isAttribute = true, localName = "allomorphAttr")
@@ -63,40 +143,111 @@ public class Suffixes
 				@JacksonXmlElementWrapper(useWrapping = false)
 				private List<Allomorph> allomorphList = new ArrayList<>();
 
-				@Getter
-				@Setter
+				public boolean isAllomorphAtribute()
+				{
+					return this.allomorphAtribute;
+				}
+
+				public String getSuffixValue()
+				{
+					return this.suffixValue;
+				}
+
+				public List<Allomorph> getAllomorphList()
+				{
+					return this.allomorphList;
+				}
+
+				@JacksonXmlProperty(isAttribute = true, localName = "allomorphAttr")
+				public void setAllomorphAtribute(final boolean allomorphAtribute)
+				{
+					this.allomorphAtribute = allomorphAtribute;
+				}
+
+				@JacksonXmlText
+				public void setSuffixValue(final String suffixValue)
+				{
+					this.suffixValue = suffixValue;
+				}
+
+				@JacksonXmlElementWrapper(useWrapping = false)
+				@JacksonXmlProperty(localName = "allomorph")
+				public void setAllomorphList(final List<Allomorph> allomorphList)
+				{
+					this.allomorphList = allomorphList;
+				}
+
 				public static class Allomorph
 				{
 					@JacksonXmlText
 					private String allomorphValue;
+
+					public String getAllomorphValue()
+					{
+						return this.allomorphValue;
+					}
+
+					@JacksonXmlText
+					public void setAllomorphValue(final String allomorphValue)
+					{
+						this.allomorphValue = allomorphValue;
+					}
 				}
 			}
 
-			@Getter
-			@Setter
 			public static class Join
 			{
 				@JacksonXmlProperty(localName = "br")
 				@JacksonXmlElementWrapper(useWrapping = false)
 				private List<Br> brList = new ArrayList<>();
+
+				public List<Br> getBrList()
+				{
+					return this.brList;
+				}
+
+				@JacksonXmlElementWrapper(useWrapping = false)
+				@JacksonXmlProperty(localName = "br")
+				public void setBrList(final List<Br> brList)
+				{
+					this.brList = brList;
+				}
 			}
 
-			@Getter
-			@Setter
 			public static class Pass
 			{
 				@JacksonXmlProperty(localName = "br")
 				@JacksonXmlElementWrapper(useWrapping = false)
 				private List<Br> brList = new ArrayList<>();
 
+				public List<Br> getBrList()
+				{
+					return this.brList;
+				}
+
+				@JacksonXmlElementWrapper(useWrapping = false)
+				@JacksonXmlProperty(localName = "br")
+				public void setBrList(final List<Br> brList)
+				{
+					this.brList = brList;
+				}
 			}
 
-			@Getter
-			@Setter
 			public static class Br
 			{
 				@JacksonXmlText
 				private String brText;
+
+				public String getBrText()
+				{
+					return this.brText;
+				}
+
+				@JacksonXmlText
+				public void setBrText(final String brText)
+				{
+					this.brText = brText;
+				}
 			}
 		}
 	}
--- src/main/java/com/smartkyc/stemmers/uzbek/UzbekStemmer.java
+++ src/main/java/com/smartkyc/stemmers/uzbek/UzbekStemmer.java
@@ -1,23 +1,23 @@
 package com.smartkyc.stemmers.uzbek;
 
+import org.slf4j.Logger;
+
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
+import java.util.stream.Collector;
 import java.util.stream.Collectors;
-
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableMap;
-import lombok.extern.slf4j.Slf4j;
 
 /**
  * Code for this class has been converted from code in this repository
  * <a href="https://github.com/MaksudSharipov/UzbekStemmer">https://github.com/MaksudSharipov/UzbekStemmer</a>
  */
-
-@Slf4j
 public class UzbekStemmer
 {
 
@@ -53,8 +53,7 @@ public class UzbekStemmer
 	private static final Object[][] MATRIX_FSM_TENSE_PERSON = new Object[][] {
 			{ 'A', 0, new int[] { 1 }, new int[] { 2, 14, 15, 16, 17, 20, 21 }, new int[] { 3, 4, 6, 7 }, new int[] { 5 },
 					new int[] { 9, 10, 12, 13 }, new int[] { 11 }, new int[] { 22 }, new int[] { 19 }, new int[] { 23, 24 }, 0, 0,
-					-1 },
-			{ 'B', 0, 0, new int[] { 8 }, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1 }, //
+					-1 }, { 'B', 0, 0, new int[] { 8 }, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1 }, //
 			{ 'C', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1 }, //
 			{ 'D', 0, 0, new int[] { 1, 2 }, 0, 0, 0, 0, 0, 0, 0, 0, 0, -2 },
 			{ 'E', 0, new int[] { 1 }, new int[] { 2, 14, 15, 16, 17 }, new int[] { 6, 7 }, 0, 0, 0, 0, new int[] { 19 }, 0,
@@ -71,8 +70,7 @@ public class UzbekStemmer
 
 	private static final Object[][] MATRIX_FSM_VERB = new Object[][] {
 			{ 'A', 0, new int[] { 1, 3, 7, 8, 9, 10, 11, 12, 13 }, new int[] { 2, 4, 5, 6, 14, 15, 16, 17, 18, 19, 20, 21, 22 },
-					new int[] { 23 }, -1 },
-			{ 'B', 0, 0, new int[] { 19 }, 0, -1 }, //
+					new int[] { 23 }, -1 }, { 'B', 0, 0, new int[] { 19 }, 0, -1 }, //
 			{ 'C', 0, 0, 0, 0, -1 }, //
 			{ 'D', 0, new int[] { 1, 3 }, new int[] { 2, 4, 5, 6 }, 0, -2 } };
 
@@ -84,30 +82,31 @@ public class UzbekStemmer
 			{ 'E', 0, new int[] { 1, 3, 4, 5, 6, 8, 10, 11 }, new int[] { 2 }, new int[] { 7, 9 }, 0, 0, -1 },
 			{ 'F', 0, new int[] { 3, 4, 6 }, 0, 0, 0, 0, -1 } };
 
-	private static final Object[][] MATRIX_FSM_DERIVATIONAL = new Object[][] {
-			{ 'A', 0, new int[] { 1 }, new int[] { 2 },
-					new int[] { 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 22, 23, 26, 27, 28, 29, 30, 31, 32,
-							33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 59,
-							60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71 },
-					new int[] { 21 }, new int[] { 24, 25 }, new int[] { 58 }, -1 },
+	private static final Object[][] MATRIX_FSM_DERIVATIONAL = new Object[][] { { 'A', 0, new int[] { 1 }, new int[] { 2 },
+			new int[] { 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 22, 23, 26, 27, 28, 29, 30, 31, 32, 33, 34,
+					35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 59, 60, 61, 62, 63,
+					64, 65, 66, 67, 68, 69, 70, 71 }, new int[] { 21 }, new int[] { 24, 25 }, new int[] { 58 }, -1 },
 			{ 'B', 0, 0, 0,
 					new int[] { 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 22, 23, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37,
-							38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50 },
-					0, 0, 0, -1 }, //
+							38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50 }, 0, 0, 0, -1 }, //
 			{ 'C', 0, 0, 0, new int[] { 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 }, 0, 0, 0, -1 }, //
 			{ 'D', 0, 0, 0, 0, 0, 0, 0, -1 }, //
 			{ 'E', 0, 0, 0, new int[] { 16, 17, 18, 19, 20 }, 0, 0, 0, -1 }, //
 			{ 'F', 0, 0, 0, new int[] { 22, 23 }, 0, 0, 0, -2 }, //
-			{ 'G', 0, 0, 0, new int[] { 22, 23, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46,
-					47, 48, 49, 50 }, 0, 0, 0, -1 } };
+			{ 'G', 0, 0, 0,
+					new int[] { 22, 23, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48,
+							49, 50 }, 0, 0, 0, -1 } };
 
 	private static final Object[][] MATRIX_FSM_NUMBER = new Object[][] {
 			{ 'A', 0, new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 }, -1 }, //
 			{ 'B', 0, 0, -1 } };
 
+	private static final Logger log = org.slf4j.LoggerFactory.getLogger(UzbekStemmer.class);
+
 	private Suffixes suffixes;
 
-	public UzbekStemmer(Function<InputStream, Suffixes> function) {
+	public UzbekStemmer(final Function<InputStream, Suffixes> function)
+	{
 		loadSuffixes(function);
 	}
 
@@ -116,7 +115,7 @@ public class UzbekStemmer
 		return stemUzbekWord(word);
 	}
 
-	private void loadSuffixes(Function<InputStream, Suffixes> function)
+	private void loadSuffixes(final Function<InputStream, Suffixes> function)
 	{
 		try (final InputStream is = UzbekStemmer.class.getResourceAsStream("/UzbekStemmer/suffixes.xml")) {
 			suffixes = function.apply(is);
@@ -205,8 +204,8 @@ public class UzbekStemmer
 			final String suffix = suffixes.getSuffixes().get(suffixesClassId - 1).getElements().get(number - 1).getElementSuffix()
 					.getSuffixValue();
 
-			if (suffix.length() <= word.length() && suffix.equals(word.substring(0, suffix.length()))
-					&& word.length() - suffix.length() > 1) {
+			if (suffix.length() <= word.length() && suffix.equals(
+					word.substring(0, suffix.length())) && word.length() - suffix.length() > 1) {
 				final String oldWord = word;
 				word = word.substring(suffix.length());
 
@@ -223,7 +222,7 @@ public class UzbekStemmer
 					continue;
 				}
 
-				final Map<String, Object> transition = ImmutableMap.of( //
+				final Map<String, Object> transition = immutableMap( //
 						SUFFIX_CLASS, "Preffixes", //
 						SUFFIX_NUMBER, number, //
 						SUFFIX, suffix, //
@@ -240,7 +239,7 @@ public class UzbekStemmer
 
 		boolean stop = false;
 		int x = 0;
-		final List<String> minWords = ImmutableList.of("u", "bu", "ol", "uq");
+		final List<String> minWords = immutableList("u", "bu", "ol", "uq");
 
 		final List<Suffixes.Suffix> allSuffixes = suffixes.getSuffixes();
 		while (x < matrixFSM.length) {
@@ -259,7 +258,7 @@ public class UzbekStemmer
 										.get(number - 1).getPass().getBrList();
 								final List<String> passesAsStrings = passes.stream() //
 										.map(Suffixes.Suffix.Element.Br::getBrText) //
-										.collect(ImmutableList.toImmutableList());
+										.collect(toImmutableList());
 								for (final String pass : passesAsStrings) {
 									if (checkSuffix(word, pass)) {
 										mistake = true;
@@ -277,7 +276,7 @@ public class UzbekStemmer
 
 									final boolean finalVal = x < 0 || !matrixFSM[x][matrixFSM[x].length - 1].equals(-2);
 
-									final Map<String, Object> transition = ImmutableMap.of( //
+									final Map<String, Object> transition = immutableMap( //
 											SUFFIX_CLASS, allSuffixes.get(suffixesClassId - 1).getSuffixClass(), //
 											SUFFIX_NUMBER, number, //
 											SUFFIX, suffix, //
@@ -289,13 +288,13 @@ public class UzbekStemmer
 											.getElements().get(number - 1).getJoin().getBrList();
 									final List<String> joinsAsStrings = joins.stream() //
 											.map(Suffixes.Suffix.Element.Br::getBrText) //
-											.collect(ImmutableList.toImmutableList());
+											.collect(toImmutableList());
 									for (final String join : joinsAsStrings) {
-										if (checkSuffix(word, join) && cutSuffix(word, join).length() > 2
-												|| minWords.contains(cutSuffix(word, join))) {
+										if (checkSuffix(word, join) && cutSuffix(word, join).length() > 2 || minWords.contains(
+												cutSuffix(word, join))) {
 											word = cutSuffix(word, join);
 
-											way.put(way.size() + 1, ImmutableMap.of( //
+											way.put(way.size() + 1, immutableMap( //
 													SUFFIX_CLASS, "Exception", //
 													SUFFIX_NUMBER, "null", //
 													SUFFIX, suffix, //
@@ -347,12 +346,13 @@ public class UzbekStemmer
 		final List<String> suffixList;
 		if (!suffixes.getSuffixes().get(suffixesClassId - 1).getElements().get(number - 1).getElementSuffix()
 				.isAllomorphAtribute()) {
-			suffixList = ImmutableList.of(suffixes.getSuffixes().get(suffixesClassId - 1).getElements().get(number - 1)
-					.getElementSuffix().getSuffixValue());
+			suffixList = immutableList(
+					suffixes.getSuffixes().get(suffixesClassId - 1).getElements().get(number - 1).getElementSuffix()
+							.getSuffixValue());
 		} else {
 			suffixList = allomorphList.stream() //
 					.map(Suffixes.Suffix.Element.ElementSuffix.Allomorph::getAllomorphValue) //
-					.collect(ImmutableList.toImmutableList());
+					.collect(toImmutableList());
 		}
 		return suffixList;
 	}
@@ -382,5 +382,37 @@ public class UzbekStemmer
 		words.add(tempWord);
 	}
 
+	@SafeVarargs
+	private static <E> List<E> immutableList(final E e, final E... others)
+	{
+		final List<E> list = new ArrayList<>(others.length + 1);
+		list.add(e);
+		list.addAll(Arrays.asList(others));
+		return Collections.unmodifiableList(list);
+	}
+
+	private static <E> Collector<E, ?, List<E>> toImmutableList()
+	{
+		return Collector.<E, ArrayList<E>, List<E>>of( //
+				ArrayList::new, //
+				ArrayList::add, //
+				(l1, l2) -> {
+					l1.addAll(l2);
+					return l1;
+				}, //
+				Collections::unmodifiableList, //
+				Collector.Characteristics.CONCURRENT);
+	}
+
+	@SuppressWarnings({ "SameParameterValue", "java:S107" })
+	private static <K, V> Map<K, V> immutableMap(K k1, V v1, K k2, V v2, K k3, V v3, K k4, V v4)
+	{
+		Map<K, V> map = new LinkedHashMap<>();
+		map.put(k1, v1);
+		map.put(k2, v2);
+		map.put(k3, v3);
+		map.put(k4, v4);
+		return Collections.unmodifiableMap(map);
+	}
 
 }
